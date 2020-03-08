c2ff773ce3e6d55cc86adab181f3ac5d5e329556
Amaury <amaury.lecomte@epitech.eu>
2020-03-08 22:19:46 +0100

    Add texts for choosing a library or a game

--- lib/src/sfml/init/SFMLGraphicalInitTextMenu.cpp
+++ lib/src/sfml/init/SFMLGraphicalInitTextMenu.cpp
@@ -13,11 +13,67 @@ static arc::SFMLText initTextArcade(const sf::Font &font)
 
     text.setFont(font);
     text.setString("Arcade");
-    text.setCharacterSize(40);
+    text.setCharacterSize(100);
     text.setFillColor(sf::Color::White);
     text.setOutlineThickness(1);
     text.setOutlineColor(sf::Color(200, 200, 200, 255));
-    text.setPosition(sf::Vector2f(100, 100));
+    text.setPosition(sf::Vector2f(750, 100));
+    return (arc::SFMLText(font, text));
+}
+
+static arc::SFMLText initTextButtonPlay(const sf::Font &font)
+{
+    sf::Text text;
+
+    text.setFont(font);
+    text.setString("Play");
+    text.setCharacterSize(30);
+    text.setFillColor(sf::Color::White);
+    text.setOutlineThickness(2);
+    text.setOutlineColor(sf::Color::Black);
+    text.setPosition(sf::Vector2f(910, 530));
+    return (arc::SFMLText(font, text));
+}
+
+static arc::SFMLText initTextButtonExit(const sf::Font &font)
+{
+    sf::Text text;
+
+    text.setFont(font);
+    text.setString("Exit");
+    text.setCharacterSize(30);
+    text.setFillColor(sf::Color::White);
+    text.setOutlineThickness(2);
+    text.setOutlineColor(sf::Color::Black);
+    text.setPosition(sf::Vector2f(910, 730));
+    return (arc::SFMLText(font, text));
+}
+
+static arc::SFMLText initTextChooseGame(const sf::Font &font)
+{
+    sf::Text text;
+
+    text.setFont(font);
+    text.setString("Choose a game");
+    text.setCharacterSize(30);
+    text.setFillColor(sf::Color::White);
+    text.setOutlineThickness(2);
+    text.setOutlineColor(sf::Color::Red);
+    text.setPosition(sf::Vector2f(1410, 330));
+    return (arc::SFMLText(font, text));
+}
+
+static arc::SFMLText initTextChooseLibrary(const sf::Font &font)
+{
+    sf::Text text;
+
+    text.setFont(font);
+    text.setString("Choose a library");
+    text.setCharacterSize(30);
+    text.setFillColor(sf::Color::White);
+    text.setOutlineThickness(2);
+    text.setOutlineColor(sf::Color::Blue);
+    text.setPosition(sf::Vector2f(190, 330));
     return (arc::SFMLText(font, text));
 }
 
@@ -27,6 +83,10 @@ void arc::SFMLGraphical::initTextMenu()
         std::vector<arc::SFMLText (*)(const sf::Font &)> text;
 
         text.push_back(initTextArcade);
+        text.push_back(initTextButtonPlay);
+        text.push_back(initTextButtonExit);
+        text.push_back(initTextChooseGame);
+        text.push_back(initTextChooseLibrary);
         return (text);
     }();
 
