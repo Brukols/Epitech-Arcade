1aa5aefeb03ca98e272b5236eaa5468cf7acbcf4
Amaury <amaury.lecomte@epitech.eu>
2020-03-15 09:14:31 +0100

    Set scale works

--- lib/sfml/src/SceneGame/display/SceneGameDisplay.cpp
+++ lib/sfml/src/SceneGame/display/SceneGameDisplay.cpp
@@ -10,18 +10,18 @@
 void arc::SceneGame::displayGame(sf::RenderWindow &window)
 {
     std::for_each(_gameMap.begin(), _gameMap.end(), [this, &window](std::shared_ptr<Entity> entity) {
-        if (entity.get()->spritePath.empty()) {
+        if (!entity.get()->spritePath.empty()) {
             sf::Sprite sprite;
             if (!_textureMap.count(entity.get()->spritePath)) {
                 _textureMap[entity.get()->spritePath].loadFromFile(entity.get()->spritePath);
             }
             sprite.setTexture(_textureMap[entity.get()->spritePath]);
-            sprite.setScale( _cell.getSize().x / 100,  _cell.getSize().y / 100);
-            sprite.setPosition(sf::Vector2f(entity.get()->x + 100, entity.get()->y + 100));
+            sprite.setScale(_cell.getSize().x / sprite.getTextureRect().width, _cell.getSize().y / sprite.getTextureRect().height);
+            sprite.setPosition(sf::Vector2f(entity.get()->x * _cell.getSize().x + 100, entity.get()->y * _cell.getSize().y + 100));
             window.draw(sprite);
         } else {
-            _cell.setPosition(sf::Vector2f(entity.get()->x * _cell.getSize().x + 100, entity.get()->y * _cell.getSize().x + 100));
-            _cell.setFillColor(sf::Color::Red);
+            _cell.setPosition(sf::Vector2f(entity.get()->x * _cell.getSize().x + 100, entity.get()->y * _cell.getSize().y + 100));
+            _cell.setFillColor(sf::Color(entity.get()->backgroundColor.r, entity.get()->backgroundColor.g, entity.get()->backgroundColor.b, entity.get()->backgroundColor.a));
             window.draw(_cell);
         }
     });
