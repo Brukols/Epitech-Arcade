6bb2a6506822ee66e05bb2db84c96dee5937b005
Céleste Bousseau <celeste@localhost.localdomain>
2020-03-17 22:51:23 +0100

    v2 score gestion

--- score/main.cpp
+++ score/main.cpp
@@ -11,73 +11,8 @@
 #include <cstring>
 #include <fstream>
 #include <ios>
-
+#include <algorithm>
 #include "score.hpp"
-
-void insert_score_file(myVector score_vector)
-{
-    std::ofstream file(SCORE_FILENAME, std::ios::out);
-    std::string line;
-    myVector::iterator it = score_vector.begin();
-
-    std::cout << "insert_score_file\n" << std::endl;
-    if(file){
-         while(it != score_vector.end())
-        {
-            file << it->first << ":" << it->second << std::endl;
-            it++;
-        }
-        file.close();
-    } else {
-        std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
-    }
-}
-
-//insert data dans vector
-myVector save_score(const std::string surname, std::string score, myVector score_vector)
-{
-    myVector::iterator it = score_vector.begin();
-
-    while(it != score_vector.end() && (std::stoi(score) <= std::stoi(it->second))) {
-        it++;
-    }
-    score_vector.insert(it, std::make_pair(surname, score));
-
-    score_vector.resize(NB_SCORE);
-    score_vector.shrink_to_fit();
-
-    std::cout << "call insert_score_file\n" << std::endl;
-    insert_score_file(score_vector);
-
-    return score_vector;
-}
-
-//met les données de score_filenema et les met dans
-myVector load_score() {
-    std::ifstream file(SCORE_FILENAME, std::ios::in);
-    std::string line;
-    myVector score_vector;
-    int cpt = 0;
-
-
-    std::string read_surname;
-    std::string read_score;
-
-    if(file)
-    {
-         while(!file.eof() && (cpt < NB_SCORE)) //not end of file
-        {
-            getline(file,read_surname,':');
-            getline(file,read_score,'\n');
-            score_vector.push_back(std::make_pair(read_surname, read_score));
-            cpt++;
-        }
-        file.close();
-    } else {
-        std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
-    }
-    return(score_vector);
-}
 
 void print_score(myVector score_vector)
 {
@@ -89,53 +24,175 @@ void print_score(myVector score_vector)
     }
 }
 
-std::vector<std::pair<std::string, std::string>> getScores()
+int verifLineFormat(std::string line)
 {
-    /*
-    ** 1 - Ouvre le fichier
-    ** 2 - Si tu n'y arrives pas -> on va throw une exception mais pour l'instant return juste un vector avec rien de dedans
-    ** 3 - Si il n'existe pas, tu le créé
-    ** 4 - Tu lis tout le fichier et tu mets dans un vector (on s'en fout pour le moment de l'ordre)
-    ** 5 - Tu trie le vector que tu as lu avec la fonction sort (Recherche comment elle se fait, tu vas surement devoir faire un overload d'opérateur mais je ne suis pas sûr)
-    ** 6 - Tu le renvoie... logique ^^
-    */
+    int length = line.size();
+
+    // recherche premier char ' " '
+    std::size_t pos = line.find_first_of('"');
+
+    // recherche premier '"'
+    if (pos != 0) {
+        std::cerr << "Format line not ok : recherche premier :" << line << std::endl;
+        return 0;
+    }
+    
+    // rechercher deuxieme '"'
+    pos = line.find_first_of('"', pos + 1);
+    if (pos == std::string::npos) {
+        std::cerr << "Format line not ok : rechercher deuxieme : " << line << std::endl;
+        return 0;
+    }
+
+    //recherche si la chaine est assez longue ("xx":"xx")
+    if (length <= pos + 4) {
+        std::cerr << "Format line not ok : line too small after the second (\"xx\":\"xx\") \n" << std::endl;
+        return 0;
+    }
+
+    //rechercher ":"
+    if (line[pos + 1] != ':') {
+        std::cerr << "Format line not ok : recherche :" << line << std::endl;
+        return 0;
+    }
+    //std::cout << "pos + 1 : " << pos + 1 << std::endl;
+
+    if (line[pos + 2] != '"') {
+        std::cerr << "Format line not ok : recherche " << line << std::endl;
+        return 0;
+    }
+
+    //recherche si il y a autre chose que des chiffres
+    pos = pos + 3;
+    while (line[pos] != line[length - 1]) {
+        if (line[pos] < '0' || line[pos] > '9') {
+            std::cerr << "Format line not ok : score is an int " << std::endl;
+            return 0;
+        }
+        pos++;
+    }
+
+    // cherche derniere '"'
+    if (line[length - 1] != '"') {
+        std::cerr << "Format line not ok : " << line << std::endl;
+        return 0;
+    }
+    return 1;
 }
 
-void insertScore(const std::string &name, const std::string &score)
+myVector sortScoreVector(myVector scoreVector)
 {
-    /*
-    ** 1 - Appelle la fonction getScores pour récupérer les scores
-    ** 2 - Une fois les scores récupérer (ils sont triés), tu insert le nouveau score
-    **     1 - Si le vector est plus grand que 10 tu supprimes le dernier et tu rajoutes le score au vector (on s'en fout de l'ordre)
-    **     2 - Si le vecotr est moins grand que 10, tu rajoutes juste le score au dernier
-    ** 3 - Tu écris le vector dans ton fichier en écrasant la données qui sont déjà insérées
-    */
+    std::sort(scoreVector.begin(), scoreVector.end(), 
+        [](const std::pair<std::string, std::string> &left, 
+           const std::pair<std::string, std::string> &right)
+        {return std::stoi(left.second) > std::stol(right.second);});
+    return(scoreVector);
 }
 
 /*
-** Pour le fichier la convention est simple: "nom":"score" (n'oublies pas de rajouter les quotes)
-** Si le fichier est mauvais, n'oublies pas de faire une gestion d'erreur en gros si tu lis: "nom":"score ce n'est pas bon
+** 1 - Ouvre le fichier
+** 2 - Si tu n'y arrives pas -> on va throw une exception mais pour l'instant return juste un vector avec rien de dedans
+** 3 - Si il n'existe pas, tu le créé (--> ca ne sert à rien de le créer il n'y aura rien dedans)
+** 4 - Tu lis tout le fichier et tu mets dans un vector (on s'en fout pour le moment de l'ordre)
+** 5 - Tu trie le vector que tu as lu avec la fonction sort (Recherche comment elle se fait, tu vas surement devoir faire un overload d'opérateur mais je ne suis pas sûr)
+** 6 - Tu le renvoie... logique ^^
 */
+myVector getScores()
+{
+    std::ifstream file(SCORE_FILENAME, std::ios::in);
+    myVector scoreVector;
+    std::size_t  pos;
+    std::string readSurname;
+    std::string readScore;
+    std::string readLine;
+    int cpt = 0;
+
+    if(file)
+    {
+         while(!file.eof() && (cpt < NB_SCORE)) //not end of file
+        {
+            getline(file, readLine);
+            if (verifLineFormat(readLine)) {
+
+                pos = readLine.find_first_of('"', 1);
+                readSurname = readLine.substr(1, pos - 1);
+                readScore = readLine.substr(pos + 3, readLine.size() - (pos + 4));
+                scoreVector.push_back(std::make_pair(readSurname, readScore));
+            } else {
+                std::cerr << "Format line not ok : " << readLine << std::endl;
+            }
+            cpt++;
+        }
+        file.close();
+    } else {
+        // throw une exception
+        std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
+    }
+    sortScoreVector(scoreVector);
+    return (scoreVector);
+    
+}
+
+void writeFile(myVector score_vector)
+{
+    std::ofstream file(SCORE_FILENAME, std::ios::out);
+    std::string line;
+    myVector::iterator it = score_vector.begin();
+
+    std::cout << "insert_score_file\n" << std::endl;
+    if(file){
+         while(it != score_vector.end())
+        {
+            file << "\"" << it->first << "\":\"" << it->second << "\"" << std::endl;
+            it++;
+        }
+        file.close();
+    } else {
+        std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
+    }
+}
 
 /*
-** Pour le nommage des variables et des fonctions, tout en camelCase ! Pas de snake_case et la PascalCase est pour le nom des classes
+** 1 - Appelle la fonction getScores pour récupérer les scores
+** 2 - Une fois les scores récupérer (ils sont triés), tu insert le nouveau score
+**     1 - Si le vector est plus grand que 10 tu supprimes le dernier et tu rajoutes le score au vector (on s'en fout de l'ordre)
+**     2 - Si le vecotr est moins grand que 10, tu rajoutes juste le score au dernier
+** 3 - Tu écris le vector dans ton fichier en écrasant la données qui sont déjà insérées
 */
+void insertScore(const std::string name, const std::string score)
+{
+    myVector scoreVector;
 
-/*
-** Autres choses, force toi à utilisez les fonctions de la librairies <alogrithm>, pour l'opti est c'est mieux
-** Par exemple, pour parcourir un vector tu vas faire un :
-** std::for_each(myVector.begin(), myVector.end(), [](std::pair<std::string, std::string) &pair) {
-** 
-** });
-*/
+    scoreVector = getScores();
+    scoreVector.push_back(std::make_pair(name, score));
+
+    print_score(scoreVector);
+
+    std::cout << "is3" << std::endl;
+
+    scoreVector = sortScoreVector(scoreVector);
+
+    print_score(scoreVector);
+
+    std::cout << "is4" << std::endl;
+    scoreVector.resize(NB_SCORE);
+    std::cout << "is5" << std::endl;
+
+    print_score(scoreVector);
+
+    scoreVector.shrink_to_fit();
+    std::cout << "is6" << std::endl;
+    writeFile(scoreVector);
+    print_score(scoreVector);
+}
 
 //sauvgarde vector dans le fichier.
 int main(int ac, char **av)
 {
     myVector score_vector;
-    int cpt = 0;
+    std::string name = "ooo";
+    std::string score = "999";
 
-    score_vector = load_score();
-    print_score(score_vector);
+    insertScore(name, score);
     return (0);
 }
